fc0c8878c340bf520f5f88f00f4b0b0bff18aa3a
krishnaju07 <92731672+krishnaju07@users.noreply.github.com>
2024-01-25 15:23:50 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from '../src/components/login'
+
+function App() {
+
+  return (
+    <>
+    <div>
+     <Login/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/login/index.jsx
@@ -0,0 +1,59 @@
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+export default function Index() {
+  const [formData, setformData] = useState({
+    user_name: null,
+    password: "",
+  });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setformData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log("Form submitted");
+  };
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h5" align="center">
+            Sign In
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            label="Username"
+            name="user_name"
+            value={formData.user_name}
+            fullWidth
+            onChange={handleChange}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            type="password"
+            label="Password"
+            name="password"
+            value={formData.password}
+            fullWidth
+            onChange={handleChange}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
