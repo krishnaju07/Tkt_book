cba21f6028c10be02bb9b33dd17af709b90a9f05
krishna <krishnamahi0703@gmail.com>
2024-01-26 11:37:06 +0530

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Login from '../src/components/login'
 import SeatBooking from '../src/components/seatBooking/'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,7 @@ function App() {
   return (
     <>
     <div>
+      <SpeedInsights/>
     <RouterProvider router={router} />
     </div>
     </>
